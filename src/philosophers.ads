83ec957eed14b358c96d5930812c3fd9c9969632
lapa44 <lapa.apiski@gmail.com>
2022-02-04 14:23:00 +0100

    Added some comments to dining philosophers solution.

--- src/philosophers.ads
+++ src/philosophers.ads
@@ -3,6 +3,7 @@ package Philosophers is
    -- Initial values
 	NO_PHILOSOPHERS : CONSTANT := 5;
 	
+	-- Protected type Fork which is securing concurrency
 	protected type Fork is
 		entry request;
 		procedure give_back;
@@ -10,11 +11,12 @@ package Philosophers is
 		is_clean : Boolean := False;
 	end Fork;
 	
+	-- task/thread of a Philosohper
 	task type Philosopher (ID: Integer; Left, Right : not null access Fork) is
 		entry start_dine;
 	end Philosopher;
 	
-	
+	-- Procedure which is executing example of dining philosophers solution
 	procedure dining_philosophers;
 
 end Philosophers;
